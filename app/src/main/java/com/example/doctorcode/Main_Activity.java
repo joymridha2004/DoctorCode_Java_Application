26064836b377b8938bbfb9daef09d2b95ab62736
Joy <joymridha2004@gmail.com>
2023-02-07 03:53:22 +0530

    github image added

--- app/src/main/java/com/example/doctorcode/Main_Activity.java
+++ app/src/main/java/com/example/doctorcode/Main_Activity.java
@@ -1,14 +1,51 @@
 package com.example.doctorcode;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatDelegate;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 public class Main_Activity extends AppCompatActivity {
+    TextView Project_Link;
+
+    ImageView github_link;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        /*<------------Night mode disable--------->*/
+
+        AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO);
+
+        /*<------------Hooks--------->*/
+
+        github_link = findViewById(R.id.github_link);
+        Project_Link = findViewById(R.id.Project_Link);
+
+        /*<------------Handle_Github_link_On_click_Listener--------->*/
+
+        github_link.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse("https://github.com/joymridha2004"));
+                startActivity(intent);
+            }
+        });
+
+        /*<------------Handle_Github_Project_Link_On_click_Listener--------->*/
+
+        Project_Link.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse("https://github.com/joymridha2004/DoctorCode_Java_Application"));
+                startActivity(intent);
+            }
+        });
     }
 }
